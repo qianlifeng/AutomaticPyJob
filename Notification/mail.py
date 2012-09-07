b73aed998b3e11e42f668e0fa6bf41ac2f548ece
qianlifeng <qianlf2008@163.com>
2012-09-07 17:15:36 +0800

    添加邮件发送功能

--- Notification/mail.py
+++ Notification/mail.py
@@ -1,7 +1,51 @@
-'''
-Created on 2012-9-7
+#!/usr/bin/env python
+# -*- coding: gbk -*-
+from __future__ import unicode_literals 
+import smtplib
+from email.mime.text import MIMEText
 
-@author: lifqian
-'''
+class Mail():
+    """
+        发送邮件
+    """
+    
+    #设置服务器，用户名、口令以及邮箱的后缀
+    mail_host = "smtp.163.com"
+    mail_user = "autorunforscott@163.com"
+    mail_pass = "autorun"
+    mail_postfix = "163.com"
+    
+    def SendMail(self, toList, title, content,fromAddress=None):
+        '''
+        toList:发给谁
+        fromAddtress:来自于
+        title:主题
+        content:内容
+        send_mail("aaa@126.com","sub","content")
+        '''
+        me = "AutorunForScott<autorunforscott@163.com>"
+        if fromAddress is None:
+            fromAddress = me
+            
+        msg = MIMEText(content)
+        msg['Subject'] = title
+        msg['From'] = fromAddress
+        msg['To'] = ";".join(toList)
+        try:
+            s = smtplib.SMTP()
+            s.connect(self.mail_host)
+            s.login(self.mail_user, self.mail_pass)
+            s.sendmail(me, toList, msg.as_string())
+            s.close()
+            return True
+        except Exception, e:
+            print str(e)
+            return False
+ 
 
-test
+
+if __name__ == "__main__":
+    if Mail().SendMail("qianlf2008@163.com", "title", "content"):
+            print "发送成功"
+    else:
+            print "发送失败"	
