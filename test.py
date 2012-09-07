039897257bb91a86a4d1d892a8ff644d6f1be825
qianlifeng <qianlf2008@163.com>
2012-09-07 13:11:23 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,17 @@
+'''
+Created on 2012-9-7
+
+@author: lifqian
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+    pass
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        pass
